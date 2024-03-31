656640279d10132bb95cd0ea8bce5c639100009a
krtresi <yuliia.prokhnich@lnu.edu.ua>
2024-04-01 02:29:51 +0300

    create js , css and html

--- quiz/js.js
+++ quiz/js.js
@@ -1,82 +1,132 @@
-const questions = [
-    {
-        question: "Яка країна відома своєю великою кількістю водоспадів?",
-        answers: [
-            { text: "Канада", correct: true },
-            { text: "Італія", correct: false },
-            { text: "Австралія", correct: false },
-            { text: "Японія", correct: false },
-        ]
-    },
-    {
-        question: "Яке місто є домівкою найвищого хмарочоса у світі, 'Бурдж Халіфа'?",
-        answers: [
-            { text: "Нью-Йорк", correct: false },
-            { text: "Пекін", correct: false },
-            { text: "Дубай", correct: true },
-            { text: "Токіо", correct: false },
-        ]
-    },
-    {
-        question: "Яке місце вважається 'столицею мистецтва' і є домівкою Лувру?",
-        answers: [
-            { text: "Рим", correct: false },
-            { text: "Барселона", correct: false },
-            { text: "Париж", correct: true },
-            { text: "Лондон", correct: false },
-        ]
-    },
-    {
-        question: "Як називається найбільший океан на Землі?",
-        answers: [
-            { text: "Атлантичний океан", correct: false },
-            { text: "Індійський океан", correct: false },
-            { text: "Тихий океан", correct: true },
-            { text: "Північний Льодовитий океан", correct: false },
-        ]
-    },
-    {
-        question: "Який вид транспорту можна використовувати для подорожей в Амазонському дощовому лісі?",
-        answers: [
-            { text: "Літак", correct: false },
-            { text: "Корабель", correct: false },
-            { text: "Ліхтарик", correct: false },
-            { text: "Каное", correct: true },
-        ]
+
+const _question = document.getElementById('question');
+const _options = document.querySelector('.quiz-options');
+const _checkBtn = document.getElementById('check-answer');
+const _playAgainBtn = document.getElementById('play-again');
+const _result = document.getElementById('result');
+const _correctScore = document.getElementById('correct-score');
+const _totalQuestion = document.getElementById('total-question');
+
+let correctAnswer = "", correctScore = askedCount = 0, totalQuestion = 5;
+
+
+async function loadQuestion(){
+    const APIUrl = 'https://opentdb.com/api.php?amount=5&category=22&difficulty=easy&type=boolean';
+    try {
+        const result = await fetch(APIUrl);
+        const data = await result.json();
+        _result.innerHTML = "";
+        showQuestion(data.results[0]);
+    } catch (error) {
+        console.error("Error loading question:", error);
+        // Затримка перед повторною спробою завантажити питання
+        setTimeout(loadQuestion, 5000); // Повторна спроба через 5 секунд
     }
-    // Додайте інші питання тут
-];
-
-const questionElement = document.getElementById("question");
-const answerButton = document.getElementById("answer-buttons");
-const nextButton = document.getElementById("next-btn");
-
-
-let currentQuestionIndex = 0;
-let score = 0;
-
-
-function startQuiz() {
-    currentQuestionIndex = 0;
-    score = 0;
-    nextButton.innerHTML = "Next";
-    showQuestion();
 }
-  
-function showQuestion() {
-    let currentQuestion = questions[currentQuestionIndex];
-    let questionNo = currentQuestionIndex + 1;
-    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
 
 
+// event listeners
+function eventListeners(){
+    _checkBtn.addEventListener('click', checkAnswer);
+    _playAgainBtn.addEventListener('click', restartQuiz);
+}
 
-    currentQuestion.answers.forEach(answer => {
-        const button = document.createElement("button");
-        button.innerHTML = answer.text;
-        button.classList.add("btn");
-        answerButton.appendChild(button);
+document.addEventListener('DOMContentLoaded', function(){
+    loadQuestion();
+    eventListeners();
+    _totalQuestion.textContent = totalQuestion;
+    _correctScore.textContent = correctScore;
+});
+
+
+// display question and options
+function showQuestion(data){
+    _checkBtn.disabled = false;
+    correctAnswer = data.correct_answer;
+    let incorrectAnswer = data.incorrect_answers;
+    let optionsList = incorrectAnswer;
+    optionsList.splice(Math.floor(Math.random() * (incorrectAnswer.length + 1)), 0, correctAnswer);
+    // console.log(correctAnswer);
+
+    _question.innerHTML = `${data.question} <br> <span class = "category"> ${data.category} </span>`;
+    _options.innerHTML = `
+        ${optionsList.map((option, index) => `
+            <li> ${index + 1}. <span>${option}</span> </li>
+        `).join('')}
+    `;
+    selectOption();
+    console.log(correctAnswer); // Додайте цей рядок
+}
+
+
+// options selection
+function selectOption(){
+    _options.querySelectorAll('li').forEach(function(option){
+        option.addEventListener('click', function(){
+            if(_options.querySelector('.selected')){
+                const activeOption = _options.querySelector('.selected');
+                activeOption.classList.remove('selected');
+            }
+            option.classList.add('selected');
+        });
     });
 }
 
+// answer checking
+function checkAnswer(){
+    _checkBtn.disabled = true;
+    if(_options.querySelector('.selected')){
+        let selectedAnswer = _options.querySelector('.selected span').textContent;
+        if(selectedAnswer == HTMLDecode(correctAnswer)){
+            correctScore++;
+            _result.innerHTML = `<p><i class = "fas fa-check"></i>Correct Answer!</p>`;
+        } else {
+            _result.innerHTML = `<p><i class = "fas fa-times"></i>Incorrect Answer!</p> <small><b>Correct Answer: </b>${correctAnswer}</small>`;
+        }
+        checkCount();
+    } else {
+        _result.innerHTML = `<p><i class = "fas fa-question"></i>Please select an option!</p>`;
+        _checkBtn.disabled = false;
+    }
+}
 
-startQuiz(); // Додати виклик функції startQuiz() для початку квізу після завантаження сторінки
+// to convert html entities into normal text of correct answer if there is any
+function HTMLDecode(textString) {
+    let doc = new DOMParser().parseFromString(textString, "text/html");
+    return doc.documentElement.textContent;
+}
+
+
+function checkCount(){
+    askedCount++;
+    setCount();
+    if(askedCount == totalQuestion){
+        setTimeout(function(){
+            console.log("");
+        }, 5000);
+
+
+        _result.innerHTML += `<p>Your score is ${correctScore}.</p>`;
+        _playAgainBtn.style.display = "block";
+        _checkBtn.style.display = "none";
+    } else {
+        setTimeout(function(){
+            loadQuestion();
+        }, 300);
+    }
+}
+
+function setCount(){
+    _totalQuestion.textContent = totalQuestion;
+    _correctScore.textContent = correctScore;
+}
+
+
+function restartQuiz(){
+    correctScore = askedCount = 0;
+    _playAgainBtn.style.display = "none";
+    _checkBtn.style.display = "block";
+    _checkBtn.disabled = false;
+    setCount();
+    loadQuestion();
+}
